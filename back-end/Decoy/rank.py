d020b5ede4706ac3b0961e47ee3b0d66776f4165
abdelhafizm <mohamed.abdel-hafiz@ucdenver.edu>
2024-09-30 16:14:51 -0600

    Updated calculate_rank()

--- back-end/Decoy/rank.py
+++ back-end/Decoy/rank.py
@@ -1,18 +1,36 @@
 import json
-
+from datetime import datetime
 
 def calculate_rank():
-    # Load the team data
-    with open("Data/TeamData.json", "r") as f:
-        team_data = json.load(f)
+    team_data = []
 
-    # Calculate the rank for each team
-    for team in team_data:
-        team["rank"] = sum(team["scores"])
+    # Load submissions
+    with open('../Data/allSubmissions.json') as file:
+        submissions = json.load(file)
 
-    # Sort the team data by rank
-    team_data.sort(key=lambda x: x["rank"], reverse=True)
+    sorted_submissions = sorted(submissions, key=lambda x: (x['time_stamp'], x['team_name']), reverse=True)
+    
+    for index, entry in enumerate(sorted_submissions):
+        team_data.append({
+            'id': index + 1,
+            'TeamName': entry['team_name'],
+            'LastSubmission': entry['time_stamp'],
+            'SuccessRate': entry['incorrect_ratio'],
+            'PerturbationMagnitude': entry['avg_l2_perturbation'],
+            'AverageConfidence': entry['avg_confidence_incorrect'],
+            'ConfidenceGap': entry['avg_confidence_gap'],
+            'VisualSimilarity': entry['avg_ssim'],
+            'TotalScore': entry['score'],
+            'Rank': 0
+        })
 
-    # Save the updated team data
-    with open("Data/TeamData.json", "w") as f:
-        json.dump(team_data, f, indent=4)
+    sorted_scores = sorted(zip([entry['TotalScore'] for entry in team_data],
+                           [datetime.strptime(entry['LastSubmission'], '%Y-%m-%d %H:%M:%S') for entry in team_data],
+                               range(len(team_data))),
+                           key=lambda x: (x[0], -x[1].month, -x[1].day), reverse=True)
+
+    for index, score in enumerate(sorted_scores):
+        team_data[score[2]]['Rank'] = index + 1
+
+    with open('../Data/TeamData.json', 'w') as outfile:
+        outfile.write(json.dumps(team_data, indent=4))
