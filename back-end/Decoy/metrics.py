abf6501ff8ae9a75a750da61e56b9d7043535ee9
abdelhafizm <mohamed.abdel-hafiz@ucdenver.edu>
2024-09-30 16:14:34 -0600

    Updated update_leaderboard()

--- back-end/Decoy/metrics.py
+++ back-end/Decoy/metrics.py
@@ -61,14 +61,45 @@ def update_evalMetrics():
 
 
 def update_leaderBoard():
-    # Load the current leaderBoard data
-    with open("Data/leaderBoard.json", "r") as f:
-        leaderBoard = json.load(f)
+    k = 5  # Number of slots in leaderboard
+    history = {}
 
-    # Update the leaderBoard data
-    leaderBoard["total"] += 1
-    leaderBoard["daily"] += 1
+    # Load submissions
+    with open('../Data/allSubmissions.json') as file:
+        submissions = json.load(file)
 
-    # Save the updated leaderBoard data
-    with open("Data/leaderBoard.json", "w") as f:
-        json.dump(leaderBoard, f, indent=4)
+    # Extract scores + timestamps, and team names
+    for entry in submissions:
+        if entry['team_name'] in history:
+            history[entry['team_name']]['score'].append(entry['score'])
+            history[entry['team_name']]['timestamp'].append(datetime.strptime(entry['time_stamp'],
+                                                                              '%Y-%m-%d %H:%M:%S'))
+        else:
+            history[entry['team_name']] = {
+                'score': [entry['score']],
+                'timestamp': [datetime.strptime(entry['time_stamp'], '%Y-%m-%d %H:%M:%S')]
+            }
+
+    # Sorting each team's stats by score then timestamp
+    teams = list(history.keys())
+    top_scores = []
+    top_timestamps = []
+    for team in teams:
+        sorted_team_metrics = sorted(zip(history[team]['score'], history[team]['timestamp']),
+                                     key=lambda x: (x[0], -x[1].month, -x[1].day), reverse=True)
+        top_scores.append(sorted_team_metrics[0][0])
+        top_timestamps.append(sorted_team_metrics[0][1])
+
+    # Sorting stats for all teams
+    sorted_overall_metrics = sorted(zip(teams, top_scores, top_timestamps),
+                                    key=lambda x: (x[1], -x[2].month, -x[2].day), reverse=True)
+
+    # Populate leaderboard with top k teams
+    leaderboard = [{'rank': index + 1,
+                    'team': sorted_overall_metrics[index][1],
+                    'score': sorted_overall_metrics[index][0]} for index in range(k)]
+
+    # Save leaderboard
+    with open('../Data/leaderboard.json', 'w') as outfile:
+        outfile.write(json.dumps(leaderboard, indent=4))
+
